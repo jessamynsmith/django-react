c18002c32a3d5ce7aecedba1722e87a6a7e3322a
jessamynsmith <geekchick77@gmail.com>
2021-12-10 14:02:37 -0700

    Added react app and included it in home page

--- django_react/settings.py
+++ django_react/settings.py
@@ -136,6 +136,9 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'frontend' / 'build',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
