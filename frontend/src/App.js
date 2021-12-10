2bcb6eeb0cdd0277d6423622e6eca19434400165
jessamynsmith <geekchick77@gmail.com>
2021-12-10 14:18:35 -0700

    Updated react app to get data from an API.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,47 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const [books, setBooks] = useState(null);
+
+  // + adding the use
+  useEffect(() => {
+    getData();
+
+    // we will use async/await to fetch this data
+    async function getData() {
+      const response = await fetch("https://www.anapioficeandfire.com/api/books");
+      const data = await response.json();
+
+      // store the data into our books variable
+      setBooks(data) ;
+    }
+  }, []); // <- you may need to put the setBooks function in this array
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>Game of Thrones Books</h1>
+      
+          {/* display books from the API */}
+          {books && (
+            <div className="books">
+      
+              {/* loop over the books */}
+              {books.map((book, index) => (
+                <div key={index}>
+                  <h2>{book.name}</h2>
+                </div>
+              ))}
+      
+            </div>
+          )}
+        </div>
       </header>
+      <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
 }
